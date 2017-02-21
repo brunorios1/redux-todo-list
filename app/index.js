68355cb4c8cb95cd792e51b236d120ea83d213a6
Bruno Rios <brunorios1@gmail.com>
2017-02-21 15:42:20 -0300

    adds todos reducer to add todos

--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var MainContainer = require('./containers/MainContainer');
+// import { createStore } from 'redux';
 
-ReactDOM.render(<MainContainer />, document.getElementById('app'))
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+            id: action.id,
+            text: action.text,
+            completed: false
+        }
+      ];
+    default:
+      return state;
+  }
+};
+
+console.log(todos([{id: 0, 'text': 'old object', 'completed': true}], {type: 'ADD_TODO', id: 1, 'text': 'Learn Redux'}));
