b3bab77dffbdfc18666ac10e8d1e2cfb2d6c51da
Bruno Rios <brunorios1@gmail.com>
2017-02-21 16:01:39 -0300

    adds TOGGLE_TODO reducer

--- app/index.js
+++ app/index.js
@@ -11,9 +11,59 @@ const todos = (state = [], action) => {
             completed: false
         }
       ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      });
     default:
       return state;
   }
 };
 
-console.log(todos([{id: 0, 'text': 'old object', 'completed': true}], {type: 'ADD_TODO', id: 1, 'text': 'Learn Redux'}));
+// ADD_TODO
+console.log(
+  todos(
+    [
+      {
+        id: 0,
+        'text': 'old object',
+        'completed': true
+      }
+    ],
+    {
+      type: 'ADD_TODO',
+      id: 1,
+      'text': 'Learn Redux'
+    }
+  )
+);
+
+// TOGGLE_TODO
+console.log(
+  todos(
+    [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Go shopping',
+        completed: false
+      }
+    ],
+    {
+      type: 'TOGGLE_TODO',
+      id: 1,
+      'completed': true
+    }
+  )
+)
